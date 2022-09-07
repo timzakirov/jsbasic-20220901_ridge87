dcf1fc492e2a62581c469e774966aa077fde3610
Timur Zakirov <timur3ak@gmail.com>
2022-09-08 00:57:16 +0300

    Основы JavaScript - решены 4 и 5 задачи

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+  if (lowerStr.includes('1xbet') || lowerStr.includes('xxx')) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let res = '';
+    for (let i = 0; i < maxlength; i++) {
+      res += str[i];
+    } 
+    return res + "...";
+  } else {
+    return str;
+  }
 }
