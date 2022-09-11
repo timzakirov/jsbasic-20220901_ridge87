5c454a0326449145614e1dc75a31cf1f110f814c
Timur Zakirov <timur3ak@gmail.com>
2022-09-11 09:50:25 +0300

    исправления 1 модуля и решение 2 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])) {
+      sum += salaries[key];
+    };
+  };
+  return sum;
 }
